dddf9791f990d678fa0c2e44c52414068c007ddf
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2024-03-24 19:44:37 -0700

    Setup Diesel

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod db;
+
 fn main() {
   tauri::Builder::default()
+    .setup(|_app| {
+      db::init();
+      Ok(())
+    })
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
